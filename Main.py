c817964344e26a6c538f88d2713392b80c3a9c6c
rphaelsi <82458288+rphaelsi@users.noreply.github.com>
2022-11-30 21:01:06 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,14 @@
-import os
+import os 
+import pyfiglet as pfg # On importe le module qui permet de faire un menu sympa
 
+#Module qui va permettre de passer des commandes systèmes.
+# Pour moi (Raphaël), le script doit être exécutée sour tous les systèmes Linux.
+# Donc première chose, c'est de passer les commandes pour installer tous les paquets.
+# On va pas encombrer le code avec ça autant faire ça avec un autre script à part.
 
+# Faudrait commenter cette partie ! Pas compris l'objectif et quel outil tu utilises !
+#Essyer de faire un petit menu sympa avec la bibliothèque pyfiglet
+#On a installer le module précèdement dans l'autre fichier (pre-script.py)
 
 print("h - help")
 print("do - tout faire sur le future DNS donné ")
@@ -14,5 +22,3 @@ elif Souhait == "do" :
     os.system("theHarvester -d "+DNS+" -b all")
 else : 
     print("Vous n'avez rien saisi.")
-
-
