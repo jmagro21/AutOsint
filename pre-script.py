65140f70e6c4ca56ded69350a0fe5ece7d6e4f90
rphaelsi <82458288+rphaelsi@users.noreply.github.com>
2022-12-06 13:54:54 +0100

    Update pre-script.py

--- pre-script.py
+++ pre-script.py
@@ -9,6 +9,8 @@ os.system("apt-get install python3")
 #Installation du module pyfiglet
 os.system("python3 -m pip install pyfiglet")
 
+# Installation de l'outil curl qui va être utile avec URLscan.io pour récupérer les résultats des scans.
+os.system("apt-get install curl")
 
 #Ci-dessous, installation de shodan et initialisation de l'API avec la clé API que l'on a récupérée sur le site shodan.
 os.system("python3 -m pip install -U --user shodan")
@@ -21,15 +23,17 @@ os.system("python3 -m pip install dnspython") # Les dépendances nécessaires à
 os.system("python3 -m pip install netaddr")
 os.system("python3 -m pip install cryptography")
 os.system("python3 -m pip install pip install packaging")
-os.system("chmod -R 700 dnscan/") # On donne les tous les droits à root et rien au reste des utilisateurs.
+os.system("mv dnscan/* ../*") # On donne les tous les droits à root et rien au reste des utilisateurs.
+os.system("rm -r dnscan/")
+os.system("chmod -R 700 ../*")
 
 #theHarvester
 os.system("apt-get install theharvester")
 
 #urlscan.io
-
+os.system("python3 -m pip install requests") # On installe les modules nécessaires pour manipuler l'API.
 
 
 #Ci-dessous, on met à jour le système et tous les paquets
-os.system("apt-get update")
-os.system("apt-get upgrade -y")
+# os.system("apt-get update")
+# os.system("apt-get upgrade -y")
